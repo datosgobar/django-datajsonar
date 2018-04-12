90892ee703723c6419b85f1ef2c98cef2433948e
Lucas Romero <lromero@devartis.com>
2018-04-12 15:41:07 -0300

    Pongo los defaults en el add_argument. Escribo la norificacion usando el stdout de Command

--- django_datajsonar/apps/management/management/commands/schedule_indexation.py
+++ django_datajsonar/apps/management/management/commands/schedule_indexation.py
@@ -6,7 +6,6 @@ from scheduler.models import RepeatableJob
 from django.core.management import BaseCommand
 from django.utils.timezone import now
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -17,26 +16,40 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('name', type=str, help='The name for the repeatable job')
-        parser.add_argument('-c', '--callable', type=str, help='importable method to be called')
-        parser.add_argument('-t', '--time', nargs=2, help='UTC hour and minutes to schedule first job, default: 6:00am')
+
+        parser.add_argument('-c', '--callable', type=str,
+                            help='importable method to be called',
+                            default='django_datajsonar.libs.indexing.tasks.schedule_new_read_datajson_task')
+
+        parser.add_argument('-t', '--time', nargs=2,
+                            help='UTC hour and minutes to schedule first job, default: 6:00am',
+                            metavar=('HOURS', 'MINUTES'),
+                            default=[6, 0]
+                            )
+
         parser.add_argument('-i', '--interval', nargs=2,
-                            help='interval and unit in which to repeat the job, default: 24 hours')
+                            help='interval and unit in which to repeat the job, default: 24 hours',
+                            metavar=('UNIT', '[weeks|days|hours|minutes]'),
+                            default=[24, 'hours']
+                            )
 
     def handle(self, *args, **options):
         # chequear que no haya un trabajo scheduleado previo
-        method = options['callable'] or 'django_datajsonar.libs.indexing.tasks.schedule_new_read_datajson_task'
-        interval = options['interval'] or (24, 'hours')
+        method = options['callable']
+        interval = options['interval']
         previously_scheduled_jobs = RepeatableJob.objects.filter(callable=method,
                                                                  interval=int(interval[0]),
                                                                  interval_unit=interval[1])
+
         if previously_scheduled_jobs:
-            print 'Ya hay un RepeatableJob registrado con ese metodo e intervalo'
+
+            self.stdout.write(u'Ya hay un RepeatableJob registrado con ese metodo e intervalo')
             return
 
         start_time = now() + datetime.timedelta(days=1)
-        time = options['time'] or (6, 0)
-        start_time = start_time.replace(hour=int(time[0]),
-                                        minute=int(time[1]),
+        time = map(int, options['time'])
+        start_time = start_time.replace(hour=time[0],
+                                        minute=time[1],
                                         second=0,
                                         microsecond=0)
 
