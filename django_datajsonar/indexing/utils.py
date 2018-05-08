c062a332fa7bf145714760b1801d60b9c929c2ce
Lucas Romero <lromero@devartis.com>
2018-05-08 13:05:51 -0300

    Considero el caso de que se cree un modelo y no contenga metadata

--- django_datajsonar/indexing/utils.py
+++ django_datajsonar/indexing/utils.py
@@ -15,8 +15,12 @@ def log_exception(task, msg, model, identifier):
 
 
 def update_model(created, trimmed_dict, model, updated_children=False, data_change=False):
-    updated = (trimmed_dict != json.loads(model.metadata) or data_change or updated_children)
-    if not created and updated:
+    try:
+        previous_meta = json.loads(model.metadata)
+    except ValueError:
+        previous_meta = {}
+    updated = (trimmed_dict != previous_meta or data_change or updated_children)
+    if created or updated:
         model.metadata = json.dumps(trimmed_dict)
         model.updated = True
     model.present = True
