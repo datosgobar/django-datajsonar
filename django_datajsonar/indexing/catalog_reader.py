920a0bf12c268d334b7b3eaeb78fa87273021928
Lucas Romero <lromero@devartis.com>
2018-05-11 11:25:44 -0300

    Cambio comportamiento donde se desindexaban datasets no presentes

--- django_datajsonar/indexing/catalog_reader.py
+++ django_datajsonar/indexing/catalog_reader.py
@@ -44,10 +44,6 @@ def index_catalog(node, task, read_local=False, whitelist=False):
         catalog_model[0].save()
 
     Dataset.objects.filter(catalog__identifier=node.catalog_id).update(present=False, updated=False, error=False)
-    # Borro de la lista de indexables a los datasets que ya no están presentes en el catálogo
-    dataset_ids = [dataset['identifier'] for dataset in catalog.get_datasets()]
-    Dataset.objects.filter(~Q(identifier__in=dataset_ids), catalog__identifier=node.catalog_id).update(indexable=False)
-
     Distribution.objects.filter(dataset__catalog__identifier=node.catalog_id).update(present=False, updated=False, error=False)
     Field.objects.filter(distribution__dataset__catalog=catalog_model).update(present=False, updated=False, error=False)
 
