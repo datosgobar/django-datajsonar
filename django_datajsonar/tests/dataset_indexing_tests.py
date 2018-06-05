6572a08e4dfa825d3e2e135bb650294ad50c3cff
Lucas Romero <lromero@devartis.com>
2018-06-05 15:45:17 -0300

    Test de la view

--- django_datajsonar/tests/dataset_indexing_tests.py
+++ django_datajsonar/tests/dataset_indexing_tests.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 
 import os
+import csv
+import io
 
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 
 from ..models import DatasetIndexingFile
 from django_datajsonar.tasks import bulk_whitelist
@@ -23,6 +27,8 @@ class BulkIndexingTests(TestCase):
     datasets_per_catalog = 3
 
     def setUp(self):
+        self.client = Client()
+        self.admin = User.objects.create_superuser('test_admin', 'myemail@test.com', 'test_pass')
         self.user = User(username='test_user', password='test_pass', email='test@email.com')
         self.user.save()
 
@@ -100,6 +106,23 @@ class BulkIndexingTests(TestCase):
         dataset = Dataset.objects.get(catalog__identifier=self.test_catalog, identifier='1')
         self.assertEqual(idx_file.state, idx_file.PROCESSED)
         self.assertTrue(dataset.indexable)
+
+    def test_config_csv_view(self):
+        filepath = os.path.join(dir_path, 'test_indexing_file.csv')
+        with open(filepath, 'rb') as f:
+            idx_file = DatasetIndexingFile(indexing_file=SimpleUploadedFile(filepath, f.read()),
+                                           uploader=self.user)
+            idx_file.save()
+            bulk_whitelist(idx_file.id)
+
+        self.client.login(username='test_admin', password='test_pass')
+        response = self.client.get(reverse('admin:config_csv'))
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        config_csv = csv.reader(io.StringIO(content))
+        csv_indexables = [row[1] for row in config_csv][1:]
+        model_indexables = list(Dataset.objects.filter(indexable=True).values_list("identifier", flat=True))
+        self.assertListEqual(sorted(csv_indexables), sorted(model_indexables))
 
     def tearDown(self):
         for catalog in self.catalogs:
