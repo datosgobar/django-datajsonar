1f2448f7c49ed300fc307cf25c17f07b02f5f7b6
Lucas Romero <lromero@devartis.com>
2018-06-13 15:31:27 -0300

    Saca el requerimiento de login para descargar el config file

--- django_datajsonar/views.py
+++ django_datajsonar/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.decorators import login_required
+#!coding=utf8
 
 from .models import Dataset
 from .utils import download_config_csv
 
 
-@login_required(login_url='/admin/login')
 def config_csv(_):
     datasets = Dataset.objects.filter(indexable=True)
     return download_config_csv(datasets)
