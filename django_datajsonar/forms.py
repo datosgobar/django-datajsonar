8c7e4371dab8b05893cced929bcdc5ec81df13c7
Lucas <lucasrafael.romero@gmail.com>
2018-08-27 10:36:48 -0300

    Agrego help text al scheduled_time

--- django_datajsonar/forms.py
+++ django_datajsonar/forms.py
@@ -7,7 +7,8 @@ from scheduler.models import RepeatableJob
 class ScheduleJobForm(forms.ModelForm):
 
     scheduled_time = forms.DateTimeField(widget=forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'}),
-                                         input_formats=['%Y-%m-%dT%H:%M'])
+                                         input_formats=['%Y-%m-%dT%H:%M'],
+                                         help_text='Formato: yyyy-mm-ddTHH:MM')
 
     class Meta:
         model = RepeatableJob
