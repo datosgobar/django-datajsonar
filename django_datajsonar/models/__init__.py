ec579117bff680440499430b5c8d50625c38036e
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-02-26 14:48:38 -0300

    Update __init__.py

--- django_datajsonar/models/__init__.py
+++ django_datajsonar/models/__init__.py
@@ -2,7 +2,7 @@
 from .utils import filepath, get_distribution_storage
 from .data_json import Catalog, Dataset, Distribution, Field
 from .metadata import Metadata
-from .tasks import ReadDataJsonTask
+from .tasks import ReadDataJsonTask, AbstractTask
 from .synchronizer import Synchronizer, Stage
 from .node import Node, NodeMetadata, NodeRegisterFile,\
     DatasetIndexingFile, Jurisdiction
