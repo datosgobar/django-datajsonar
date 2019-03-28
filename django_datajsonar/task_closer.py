d81cd5e93680b5a99e0218490588d71b1eed5e67
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-03-28 17:16:58 -0300

    Agrego chequeo de que se cierren solo las tareas finalizadas

--- django_datajsonar/task_closer.py
+++ django_datajsonar/task_closer.py
@@ -1,10 +1,13 @@
 from django_datajsonar.models import AbstractTask
+from django_datajsonar.utils import pending_or_running_jobs
 
 
 class TaskCloser(object):
 
-    def __init__(self, task_model):
+    def __init__(self, task_model, does_queue_have_running_jobs=pending_or_running_jobs):
+        self.does_queue_have_running_jobs = does_queue_have_running_jobs
         self.task_model = task_model
 
     def close_all_opened(self):
-        self.task_model.objects.update(status=AbstractTask.FINISHED)
+        if not self.does_queue_have_running_jobs('a_queue'):
+            self.task_model.objects.update(status=AbstractTask.FINISHED)
--- django_datajsonar/tests/task_closer_tests.py
+++ django_datajsonar/tests/task_closer_tests.py
@@ -11,6 +11,11 @@ class TaskCloserTests(TestCase):
     def test_close_all_updates_all_models(self):
 
         mock_task = Mock()
-        TaskCloser(mock_task).close_all_opened()
+        TaskCloser(mock_task, does_queue_have_running_jobs=Mock(return_value=False)).close_all_opened()
 
         mock_task.objects.update.assert_called_with(status=AbstractTask.FINISHED)
+
+    def test_close_all_does_not_close_if_tasks_are_running(self):
+        mock_task = Mock()
+        TaskCloser(mock_task, does_queue_have_running_jobs=Mock(return_value=True)).close_all_opened()
+        mock_task.objects.update.assert_not_called()
