96b4c3db2817d82b36fb346e3feabd45ef6b9fe4
Lucas Romero <lromero@devartis.com>
2019-05-10 18:03:18 -0300

    Creo los modelos de metadata

--- django_datajsonar/models/__init__.py
+++ django_datajsonar/models/__init__.py
@@ -1,7 +1,7 @@
 #! coding: utf-8
 from .utils import filepath, get_distribution_storage
 from .data_json import Catalog, Dataset, Distribution, Field
-from .metadata import Metadata
+from .metadata import Metadata, ProjectMetadata, Language, Publisher, Spatial
 from .tasks import ReadDataJsonTask, AbstractTask
 from .synchronizer import Synchronizer
 from .stage import Stage
