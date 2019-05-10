1a8b3563494259e9b27f6fca4f1a1a3212df7f36
Lucas Romero <lromero@devartis.com>
2019-05-10 18:07:57 -0300

    Agrego view para mostrar la metadata de los nodos

--- django_datajsonar/views.py
+++ django_datajsonar/views.py
@@ -1,9 +1,20 @@
 #!coding=utf8
+from django.http import JsonResponse
 
-from .models import Dataset
-from .utils.utils import download_config_csv
+from django_datajsonar.models.data_json import Dataset
+from django_datajsonar.utils.metadata_generator import project_metadata, \
+    jurisdiction_list_metadata
+from django_datajsonar.utils.utils import download_config_csv
 
 
 def config_csv(_):
     datasets = Dataset.objects.filter(indexable=True)
     return download_config_csv(datasets)
+
+
+def nodes_metadata_json(_):
+    response = {'meta': project_metadata(),
+                'jurisdictions': jurisdiction_list_metadata()}
+    return JsonResponse(response)
+
+
--- urls.py
+++ urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django_datajsonar.views import nodes_metadata_json
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^django-rq/', include('django_rq.urls')),
+    url(r'^nodes.json/', nodes_metadata_json)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
