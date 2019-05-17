99966bd1045d86bb7f3bb47119ed27d828963dbc
Lucas <lromero@devartis.com>
2019-05-17 12:04:18 -0300

    cierro los regex de las urls

--- django_datajsonar/urls.py
+++ django_datajsonar/urls.py
@@ -4,7 +4,7 @@ from django_datajsonar.views import nodes_metadata_json,\
     nodes_english_metadata_csv, nodes_spanish_metadata_csv
 
 urlpatterns = [
-    url(r'^nodes.json/', nodes_metadata_json),
-    url(r'^nodes.csv/', nodes_english_metadata_csv),
-    url(r'^nodos.csv/', nodes_spanish_metadata_csv),
+    url(r'^nodes.json/$', nodes_metadata_json),
+    url(r'^nodes.csv/$', nodes_english_metadata_csv),
+    url(r'^nodos.csv/$', nodes_spanish_metadata_csv),
 ]
