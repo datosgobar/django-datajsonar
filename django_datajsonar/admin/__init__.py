011e3550aa22368635a449232c9d708a3ba89370
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-28 16:36:15 -0300

    Agrego IndexingConfig al admin

--- django_datajsonar/admin/__init__.py
+++ django_datajsonar/admin/__init__.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from django_datajsonar.models import Jurisdiction
 from django_datajsonar.models import Stage
-from . import synchronizer, data_json, metadata, node, repeatable_job, tasks
+from . import synchronizer, data_json, metadata, node, repeatable_job, tasks, config
 
 admin.site.register(Jurisdiction)
 admin.site.register(Stage)
