ccd25dcf92cb31c1ebc6493ea934b200597e11b1
Lucas Romero <lromero@devartis.com>
2019-05-31 17:57:28 -0300

    Agrego routeo para la nueva view

--- django_datajsonar/urls.py
+++ django_datajsonar/urls.py
@@ -2,7 +2,8 @@
 from django.conf.urls import url
 from django_datajsonar.views import nodes_metadata_json, \
     nodes_english_metadata_csv, nodes_spanish_metadata_csv, \
-    nodes_english_metadata_xlsx, nodes_spanish_metadata_xlsx
+    nodes_english_metadata_xlsx, nodes_spanish_metadata_xlsx, \
+    distributions_spanish_metadata_csv
 
 urlpatterns = [
     url(r'^nodes.json/$', nodes_metadata_json),
@@ -10,4 +11,5 @@ urlpatterns = [
     url(r'^nodos.csv/$', nodes_spanish_metadata_csv),
     url(r'^nodes.xlsx/$', nodes_english_metadata_xlsx),
     url(r'^nodos.xlsx/$', nodes_spanish_metadata_xlsx),
+    url(r'^distribuciones.csv/$', distributions_spanish_metadata_csv),
 ]
