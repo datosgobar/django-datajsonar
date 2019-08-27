9ec3ab156cef788784d03c0d69a45958819dbe6f
Franco Scaccheri Cassanello <fcassanello@devartis.com>
2019-08-27 16:46:31 -0300

    Agrego list filter para nuevo atributo starred

--- django_datajsonar/admin/data_json.py
+++ django_datajsonar/admin/data_json.py
@@ -72,7 +72,7 @@ class DatasetAdmin(admin.ModelAdmin):
     actions = ['make_indexable', 'make_unindexable', 'generate_config_file',
                'mark_as_reviewed', 'mark_on_revision', 'mark_as_not_reviewed']
 
-    list_filter = ('catalog__identifier', 'present', 'indexable', 'reviewed')
+    list_filter = ('catalog__identifier', 'present', 'indexable', 'reviewed', 'starred')
     list_select_related = True
 
     class Media:
