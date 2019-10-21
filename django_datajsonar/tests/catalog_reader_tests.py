793517e6a6b8d7fd3105cce5c13068f6222b595b
fcassanello <fcassanello@gitlab.com>
2019-10-21 14:55:09 -0300

    Agrego test de indexacion de catalogos

--- django_datajsonar/tests/catalog_reader_tests.py
+++ django_datajsonar/tests/catalog_reader_tests.py
@@ -3,7 +3,9 @@ from unittest import mock
 
 from django.test import TestCase
 
+from conf.settings.base import MEDIA_ROOT
 from django_datajsonar.indexing.catalog_reader import CatalogReader
+from django_datajsonar.indexing.constants import CATALOG_ROOT
 from django_datajsonar.models import Node, ReadDataJsonTask
 from django_datajsonar.models.config import IndexingConfig
 from django_datajsonar.tests.helpers import create_node
@@ -33,3 +35,13 @@ class CatalogReaderTests(TestCase):
         task = ReadDataJsonTask.objects.create()
         CatalogReader().index(node, task)
         self.assertEqual(database_loader.call_args[1]['verify_ssl'], True)
+
+    def test_catalog_indexation_creates_catalog_files(self, database_loader):
+        node = create_node('sample_data.json')
+        task = ReadDataJsonTask.objects.create()
+        json_file_path = os.path.join(MEDIA_ROOT, CATALOG_ROOT, node.catalog_id, 'data.json')
+        xlsx_file_path = os.path.join(MEDIA_ROOT, CATALOG_ROOT, node.catalog_id, 'catalog.xlsx')
+
+        CatalogReader().index(node, task)
+        self.assertTrue(os.path.exists(json_file_path))
+        self.assertTrue(os.path.exists(xlsx_file_path))
