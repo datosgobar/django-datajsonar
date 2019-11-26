cda3b5f95ad0124e4948c5b1b1f3ef2c0cc0c9dc
Lucas Romero <lromero@devartis.com>
2019-11-26 11:18:13 -0300

    Tests de verify_ssl en la generación de archivos de catalogo

--- django_datajsonar/tests/catalog_reader_tests.py
+++ django_datajsonar/tests/catalog_reader_tests.py
@@ -1,14 +1,16 @@
 import os
 from unittest import mock
 
+import requests_mock
 from django.conf import settings
 from django.test import TestCase
 
 from django_datajsonar.indexing.catalog_reader import CatalogReader
 from django_datajsonar.indexing.constants import CATALOG_ROOT
-from django_datajsonar.models import ReadDataJsonTask
+from django_datajsonar.models import ReadDataJsonTask, Node
 from django_datajsonar.models.config import IndexingConfig
-from django_datajsonar.tests.helpers import create_node
+from django_datajsonar.tests.helpers import create_node, open_catalog
+from django_datajsonar.utils.catalog_file_generator import CatalogFileGenerator
 
 
 @mock.patch('django_datajsonar.indexing.catalog_reader.DatabaseLoader')
@@ -45,3 +47,47 @@ class CatalogReaderTests(TestCase):
         CatalogReader().index(node, task)
         self.assertTrue(os.path.exists(json_file_path))
         self.assertTrue(os.path.exists(xlsx_file_path))
+
+    def test_catalog_url_request_does_not_verify_ssl_by_default(self, _database_loader):
+        node = Node.objects.create(catalog_id='test_catalog',
+                                   catalog_format='json',
+                                   catalog_url='https://fakeurl.com/data.json',
+                                   indexable=True)
+        with open_catalog('sample_data.json') as sample:
+            text = sample.read()
+        with requests_mock.Mocker() as m:
+            m.get('https://fakeurl.com/data.json', status_code=200, content=text)
+            CatalogFileGenerator(node).generate_files()
+            self.assertEqual(2, len(m.request_history))
+            for request in m.request_history:
+                self.assertTrue(request.verify)
+
+    def test_catalog_url_request_verifies_ssl_according_to_node(self, _database_loader):
+        node = Node.objects.create(catalog_id='test_catalog',
+                                   catalog_format='json',
+                                   catalog_url='https://fakeurl.com/data.json',
+                                   indexable=True,
+                                   verify_ssl=False)
+        with open_catalog('sample_data.json') as sample:
+            text = sample.read()
+        with requests_mock.Mocker() as m:
+            m.get('https://fakeurl.com/data.json', status_code=200, content=text)
+            CatalogFileGenerator(node).generate_files()
+            self.assertEqual(2, len(m.request_history))
+            for request in m.request_history:
+                self.assertFalse(request.verify)
+
+    def test_catalog_url_request_verifies_ssl_on_enabled_node(self, _database_loader):
+        node = Node.objects.create(catalog_id='test_catalog',
+                                   catalog_format='json',
+                                   catalog_url='https://fakeurl.com/data.json',
+                                   indexable=True,
+                                   verify_ssl=True)
+        with open_catalog('sample_data.json') as sample:
+            text = sample.read()
+        with requests_mock.Mocker() as m:
+            m.get('https://fakeurl.com/data.json', status_code=200, content=text)
+            CatalogFileGenerator(node).generate_files()
+            self.assertEqual(2, len(m.request_history))
+            for request in m.request_history:
+                self.assertTrue(request.verify)
